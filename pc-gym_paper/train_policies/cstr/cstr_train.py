e71c35493b498da0a780d4d2e173fa8add5acba3
Max Bloor <max.bloor@gmail.com>
2024-10-03 09:42:05 +0100

    formatting cstr_train.py

--- pc-gym_paper/train_policies/cstr/cstr_train.py
+++ pc-gym_paper/train_policies/cstr/cstr_train.py
@@ -6,7 +6,7 @@ from pcgym import make_env
 from callback import LearningCurveCallback
 import numpy as np
 from stable_baselines3 import PPO, DDPG, SAC
-
+from custom_reward import sp_track_reward
 
 # Define environment
 T = 26
@@ -29,43 +29,6 @@ observation_space = {
 r_scale = {'Ca':1e3}
 
 # Define reward to be equal to the OCP (i.e the same as the oracle)
-def oracle_reward(self,x,u,con):
-    Sp_i = 0
-    cost = 0 
-    R = 0.1
-    if not hasattr(self, 'u_prev'):
-        self.u_prev = u
-
-    for k in self.env_params["SP"]:
-        i = self.model.info()["states"].index(k)
-        SP = self.SP[k]
-        
-        o_space_low = self.env_params["o_space"]["low"][i] 
-        o_space_high = self.env_params["o_space"]["high"][i] 
-
-        x_normalized = (x[i] - o_space_low) / (o_space_high - o_space_low)
-        setpoint_normalized = (SP - o_space_low) / (o_space_high - o_space_low)
-
-        r_scale = self.env_params.get("r_scale", {})
-
-        cost += (np.sum(x_normalized - setpoint_normalized[self.t]) ** 2) * r_scale.get(k, 1)
-
-        Sp_i += 1
-    u_normalized = (u - self.env_params["a_space"]["low"]) / (
-        self.env_params["a_space"]["high"] - self.env_params["a_space"]["low"]
-    )
-    u_prev_norm =  (self.u_prev - self.env_params["a_space"]["low"]) / (
-        self.env_params["a_space"]["high"] - self.env_params["a_space"]["low"]
-    )
-    self.u_prev = u
-
-    # Add the control cost
-    cost += np.sum(R * (u_normalized-u_prev_norm)**2)
-    r = -cost
-    try:
-        return r[0]
-    except Exception:
-        return r
 env_params = {
     'N': nsteps, 
     'tsim':T, 
@@ -80,7 +43,7 @@ env_params = {
     'noise':True, 
     'integration_method': 'casadi', 
     'noise_percentage':0.001, 
-    'custom_reward': oracle_reward
+    'custom_reward': sp_track_reward
 }
 
 env = make_env(env_params)
